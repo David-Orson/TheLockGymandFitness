a718027e392775792bf44e354c79aa67b0acbb70
David Orson <onlyorson@outlook.com>
2020-09-24 09:14:58 +0100

    About

--- src/Sections/About.js
+++ src/Sections/About.js
@@ -6,12 +6,20 @@ const About = () => {
   return (
     <div id="about" class="about">
       <h2>About Us</h2>
-      <div>
-        <h4>Local Worksop Gym</h4>
-        <p>
-          Lorem asdf asdf asd fas dfa sdf asdf asdf asdf asdf asdf asdf asd fas
-          dfa sdf{" "}
-        </p>
+      <div className="container">
+        <div className="content">
+          <h4>Local Worksop Gym</h4>
+          <p>
+            The Lock Gym and Fitness is a local Worksop Gym in the centre of
+            Worksop located close to the town centre and train station. At The
+            Lock we are cultivating a friendly atmosphere where we can help our
+            customers achieve their fitness goals! The Lock has recently
+            exchanged ownership in July 2020 and we are committed to customer
+            service. We are gym suitable for everyone, we have members from
+            competitive bodybuilders to casual gym goers. Whoever you are, you
+            will find a home at The Lock Gym and Fitness.
+          </p>
+        </div>
       </div>
     </div>
   );
