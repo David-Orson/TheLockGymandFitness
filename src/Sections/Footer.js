681b8278b906c94a920c1499237e8d753eab9f18
David Orson <onlyorson@outlook.com>
2020-11-16 00:46:03 +0000

    Footer

--- src/Sections/Footer.js
+++ src/Sections/Footer.js
@@ -2,8 +2,18 @@ import React from "react";
 
 import "../css/Footer.css";
 
+import copy from '../Assets/Icons/copyright.png'
+
 const Footer = () => {
-  return <div class="footer"></div>;
+
+  
+
+  return <div class="footer">
+    <p>The Lock Gym and Fitness</p>
+    <p>Bridge Place</p>
+    <p>Cuckoo Wharf, Worksop S80 1DT</p>
+    <p>Copyright <img className="copyright-symbol" src={copy} alt="copyright symbol" /> 2020 The Lock Gym and Fitness, all rights reserved.</p>
+  </div>;
 };
 
 export default Footer;
