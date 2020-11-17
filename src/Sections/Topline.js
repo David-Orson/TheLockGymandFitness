f6720e26860bdb442786a0290edd682536e9a844
David Orson <onlyorson@outlook.com>
2020-11-17 18:19:33 +0000

    navbar animation and design changes

--- src/Sections/Topline.js
+++ src/Sections/Topline.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 
 import logo from "../Assets/LockLogo1.png";
 
@@ -7,14 +7,27 @@ import Stripes from "../Assets/accents/AngledStripes.png"
 import "../css/Topline.css";
 
 const Topline = () => {
+  const [navbar, setNavBar] = useState(false);
+
+  const changeBackground = () => {
+    if (window.scrollY >= 30) {
+      setNavBar(true)
+    } else {
+      setNavBar(false)
+    }
+  }
+
+  window.addEventListener('scroll', changeBackground);
+
   return (
-    <div class="topline">
+    
+    <div class={`topline ${navbar === true ? ('scrolled') : (null)}`}>
       <img className="logo" src={logo} alt="The Lock Gym and Fitness" />
       <div className="links">
-        <a href="#top">Top</a>
-        <a href="#about">About</a>
-        <a href="#trainers">Trainers</a>
-        <a href="#gallery">Gallery</a>
+        <a className="link" href="#top">Top</a>
+        <a className="link" href="#about">About</a>
+        <a className="link" href="#trainers">Trainers</a>
+        <a className="link" href="#gallery">Gallery</a>
         
       </div>
       <img className="topline-stripes" src={Stripes} alt="Pink Stripes" />
